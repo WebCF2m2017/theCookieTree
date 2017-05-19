9cc85f26d73201a8fed585fe50cf2e9b16c5585c
joao <joao_mnstrkhaos@hotmail.com>
2017-05-19 15:47:37 +0200

    implantation de menu.php

---
+++ menu.php
@@ -0,0 +1,11 @@
+<div class="container">
+		<nav class="navbar navbar-inverse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="./">Accueil</a></li>
+				<li><a href="?entreprise">Entreprise</a></li>
+				<li><a href="?produits">Produits</a></li>
+				<li><a href="?contact">Contact</a></li>
+				<li><a href="?galerie">Galerie</a></li>
+			</ul>
+		</nav>
+	</div>
