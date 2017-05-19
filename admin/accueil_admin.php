c930986d09a00693d918c479ce5059bbd3b33381
Mounir52 <koga.gennosuke@hotmail.com>
2017-05-19 15:06:37 +0200

    formulaire connexion

---
+++ admin/accueil_admin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Présentation</title>
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<!-- Lien vers la librairie d'icones Font Awesome 4.7.0 -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+	<title>Admin</title>
+</head>
+
+<body>
+
+	<div class="container">
+		<form action="" method="POST" class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-8 col-sm-offset-2 col-xs-12 form-horizontal ">
+	<div class="row">
+<div class="form-group">
+<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-8 col-sm-offset-2 col-xs-12 form-horizontal">
+    <label for="prenom">Username <span class="required">*</span></label>
+    <br />
+    <input type="text" id="prenom" class=" form-control field-divided col-lg-6 col-md-6 col-sm-6 col-xs-6" placeholder="First" />
+    <br /><br />
+    <label for="prenom">password <span class="required">*</span></label>
+    <br />
+
+    <input type="text" class=" form-control field-divided col-lg-6 col-md-6 col-sm-6 col-xs-6" placeholder="Last" /><br />
+</div>
+</div>
+</div>
+	
+
+	<div class="row">
+	<div class="form-group">
+	<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-8 col-sm-offset-2 col-xs-12 form-horizontal">
+	
+        	<input type="submit" class="form-control btn btn-primary" value="Connexion"  />
+    </div>
+	</div>
+	</div>
+	</div>    
+		</form>
+	</div>
+
+</body>
+</html>
+
