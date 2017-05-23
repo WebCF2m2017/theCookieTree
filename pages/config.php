60c1d5beb92acd165b1f45ee033c5a8264067be9
joao <joao_mnstrkhaos@hotmail.com>
2017-05-23 14:10:53 +0200

    changement de base de donnes

--- pages/config.php
+++ pages/config.php
@@ -3,5 +3,5 @@
 define("DB_HOST", "localhost");
 define("DB_LOGIN", "root");
 define("DB_PWD", "");
-define("DB_NAME", "mydb");
+define("DB_NAME", "theCookieTree");
 define("DB_CHARSET", "utf8"); 
--- pages/galerie.php
+++ pages/galerie.php
@@ -11,6 +11,7 @@ $sql = "SELECT i.id, i.url, i.produits_id, p.categ_id, p.description, p.titre, p
     INNER JOIN categ c
         ON c.id = p.categ_id
     WHERE p.categ_id = 1
+        ORDER BY i.produits_id ASC;
     ";
 
 $recup_sql = mysqli_query($db, $sql)or die(mysqli_error($db));
@@ -32,6 +33,7 @@ $sql = "SELECT i.id, i.url, i.produits_id, p.categ_id, p.description, p.titre, p
     INNER JOIN categ c
         ON c.id = p.categ_id
     WHERE p.categ_id = 2
+        ORDER BY i.produits_id ASC;
     ";
 
 $recup_sql2 = mysqli_query($db, $sql)or die(mysqli_error($db));
@@ -66,11 +68,11 @@ if(!mysqli_num_rows($recup_sql2)){
     </div>
 
     <!-- Gluten free -->
-    <section class="col-md-12">
-
+  
     <div class="jumbotron">
       <h3><?=$gluten['types']?></h3>
     </div>
+    <section class="col-md-12">
 
     <div class="row" id="galerie">
       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -102,34 +104,49 @@ if(!mysqli_num_rows($recup_sql2)){
                <?php
                }
               ?>
+        </section>
     <!-- end of Gluten free -->
 
 
     <!-- Vegan products -->
+    <?php
+            while ($vegan = mysqli_fetch_assoc($recup_sql2)){
+            ?>
+     <div class="jumbotron">
+      <h3><?=$vegan['types']?></h3>
+    </div>
     <div class="row" id="galerie">
       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+             <section class="col-md-12">
+            <?php if(isset($erreur)){ echo "<h3>$erreur</h3>"; }else{ ?> 
 
-       
-           <h3>Barre latérale</h3>
+             <h3><?=$vegan['titre']?></h3>
 
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dicta fugiat modi fugit, ex, aspernatur ipsam! Odio, quidem odit ipsa porro nesciunt a! Voluptates ipsum, voluptas sit ea laudantium voluptate.</p>
+           <?php
+            }
+            ?>
+            
+              <?php if(isset($erreur2)){ echo "<p>$erreur</p>"; }else{ ?> 
+
+                <p><?php echo nl2br($vegan['description'])?></p>
+              
+              <?php
+               }
+              ?>
       </div>
+         <?php
+            while ($vegan = mysqli_fetch_assoc($recup_sql2)){
+            ?>
+
       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
 
-        <img src="https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/17523371_1883794808545417_5979494171983979335_n.jpg?oh=7b47f34eaf37dd96d9716ba4dc441628&oe=59B9ACC0" class="img-responsive img-thumbnail" alt="Tulipes" title="Tulipes" />
-
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-
-        <img src="https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/17523371_1883794808545417_5979494171983979335_n.jpg?oh=7b47f34eaf37dd96d9716ba4dc441628&oe=59B9ACC0" class="img-responsive img-thumbnail" alt="Tulipes" title="Tulipes" />
-
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-
-        <img src="https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/17523371_1883794808545417_5979494171983979335_n.jpg?oh=7b47f34eaf37dd96d9716ba4dc441628&oe=59B9ACC0" class="img-responsive img-thumbnail" alt="Tulipes" title="Tulipes" />
-
-      </div>
+        <img src="<?=$vegan['url']?>" class="img-responsive img-thumbnail" alt="Tulipes" title="Tulipes" />
+        
     </div>
+         </section>       
+              <?php
+               }
+              ?>
     <!-- end of Vegan products -->
 	  
   </body>
