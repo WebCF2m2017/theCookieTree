1f880697427c31bd4a2709543f3255a583acd35f
joao <joao_mnstrkhaos@hotmail.com>
2017-06-16 15:50:43 +0200

    nouvelle base de donnes

---
+++ pages/config_online.php
@@ -0,0 +1,7 @@
+<?php
+	define("DB_HOST", "sqlgold.phpnet.org:48614");
+	define("DB_LOGIN", "sql8614_12");
+	define("DB_PWD", "6uTj3dodg03h");
+	define("DB_NAME","theCookieTree");
+	define("DB_CHARSET","utf8");
+?>
